a37d6a312fd8a31e74e53a77bd5365da9311984b
Cosmo Zhang <cosmozhang1988@gmail.com>
2015-03-16 17:04:10 -0400

    dataprocess

--- dl/process_data.py
+++ dl/process_data.py
@@ -1,66 +1,55 @@
 import numpy as np
-import cPickle
+import cPickle as cpcl
 from collections import defaultdict
-import sys, re
+import sys
+import re
 import pandas as pd
+from nltk.tokenize import word_tokenize
 
-def build_data_cv(data_folder, cv=10, clean_string=True):
+
+def build_data_cv(filename, cv=10, speaker = True):
     """
     Loads data and split into 10 folds.
     """
-    revs = []
-    pos_file = data_folder[0]
-    neg_file = data_folder[1]
+    quos = []
     vocab = defaultdict(float)
-    with open(pos_file, "rb") as f:
-        for line in f:       
-            rev = []
-            rev.append(line.strip())
-            if clean_string:
-                orig_rev = clean_str(" ".join(rev))
+    with open(filename, "rb") as f:
+        quodata = cpcl.load(f)
+        for eg in quodata:
+            if speaker:
+                wlist = clean_str(eg[0] + ' ' +' '.join(eg[1]))
             else:
-                orig_rev = " ".join(rev).lower()
-            words = set(orig_rev.split())
-            for word in words:
+                wlist = clean_str(' '.join(eg[1]))
+            for word in wlist:
                 vocab[word] += 1
-            datum  = {"y":1, 
-                      "text": orig_rev,                             
-                      "num_words": len(orig_rev.split()),
-                      "split": np.random.randint(0,cv)}
-            revs.append(datum)
-    with open(neg_file, "rb") as f:
-        for line in f:       
-            rev = []
-            rev.append(line.strip())
-            if clean_string:
-                orig_rev = clean_str(" ".join(rev))
-            else:
-                orig_rev = " ".join(rev).lower()
-            words = set(orig_rev.split())
-            for word in words:
-                vocab[word] += 1
-            datum  = {"y":0, 
-                      "text": orig_rev,                             
-                      "num_words": len(orig_rev.split()),
-                      "split": np.random.randint(0,cv)}
-            revs.append(datum)
-    return revs, vocab
-    
+            if eg[-1] == "pos":
+                    y = 1
+            elif eg[-1] == "neg":
+                    y = 0
+            datum = {"y": 1,
+                     "text": wlist,
+                     "num_words": len(wlist),
+                     "split": np.random.randint(0, cv)}
+            quos.append(datum)
+    return quos, vocab
+
+
 def get_W(word_vecs, k=300):
     """
     Get word matrix. W[i] is the vector for word indexed by i
     """
-    vocab_size = len(word_vecs)
+    vocab_size = len(word_vecs) #the updated vocab_size
     word_idx_map = dict()
-    W = np.zeros(shape=(vocab_size+1, k))            
-    W[0] = np.zeros(k)
-    i = 1
+    W = np.zeros(shape=(vocab_size + 1, k)) #W is the word matrix
+    W[0] = np.ones(k)
+    i = 1 #W[0] is the bias intercept
     for word in word_vecs:
         W[i] = word_vecs[word]
         word_idx_map[word] = i
         i += 1
     return W, word_idx_map
 
+#function to read-in the word vector bin file
 def load_bin_vec(fname, vocab):
     """
     Loads 300x1 word vecs from Google (Mikolov) word2vec
@@ -78,69 +67,85 @@ def load_bin_vec(fname, vocab):
                     word = ''.join(word)
                     break
                 if ch != '\n':
-                    word.append(ch)   
+                    word.append(ch)
             if word in vocab:
-               word_vecs[word] = np.fromstring(f.read(binary_len), dtype='float32')  
+                word_vecs[word] = np.fromstring(
+                    f.read(binary_len), dtype='float32')
             else:
                 f.read(binary_len)
     return word_vecs
 
+
 def add_unknown_words(word_vecs, vocab, min_df=1, k=300):
     """
-    For words that occur in at least min_df documents, create a separate word vector.    
+    For words that occur in at least min_df documents, create a separate word vector.
     0.25 is chosen so the unknown vectors have (approximately) same variance as pre-trained ones
     """
     for word in vocab:
-        if word not in word_vecs and vocab[word] >= min_df:
-            word_vecs[word] = np.random.uniform(-0.25,0.25,k)  
+        if word not in word_vecs and vocab[word] >= min_df: #min_df is the minimum appearance count
+            word_vecs[word] = np.random.uniform(-0.25, 0.25, k)
+
 
 def clean_str(string, TREC=False):
+    wlst = word_tokenize(string)
+    if TREC:
+        return wlst
+    else:
+        # return [word.lower() for word in wlst]
+        return [word for word in wlst]
+
     """
     Tokenization/string cleaning for all datasets except for SST.
     Every dataset is lower cased except for TREC
+
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\'s", " \'s", string)
+    string = re.sub(r"\'ve", " \'ve", string)
+    string = re.sub(r"n\'t", " n\'t", string)
+    string = re.sub(r"\'re", " \'re", string)
+    string = re.sub(r"\'d", " \'d", string)
+    string = re.sub(r"\'ll", " \'ll", string)
+    string = re.sub(r",", " , ", string)
+    string = re.sub(r"!", " ! ", string)
+    string = re.sub(r"\(", " \( ", string)
+    string = re.sub(r"\)", " \) ", string)
+    string = re.sub(r"\?", " \? ", string)
+    string = re.sub(r"\s{2,}", " ", string)
     """
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)     
-    string = re.sub(r"\'s", " \'s", string) 
-    string = re.sub(r"\'ve", " \'ve", string) 
-    string = re.sub(r"n\'t", " n\'t", string) 
-    string = re.sub(r"\'re", " \'re", string) 
-    string = re.sub(r"\'d", " \'d", string) 
-    string = re.sub(r"\'ll", " \'ll", string) 
-    string = re.sub(r",", " , ", string) 
-    string = re.sub(r"!", " ! ", string) 
-    string = re.sub(r"\(", " \( ", string) 
-    string = re.sub(r"\)", " \) ", string) 
-    string = re.sub(r"\?", " \? ", string) 
-    string = re.sub(r"\s{2,}", " ", string)    
-    return string.strip() if TREC else string.strip().lower()
 
 def clean_str_sst(string):
     """
     Tokenization/string cleaning for the SST dataset
     """
-    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)   
-    string = re.sub(r"\s{2,}", " ", string)    
+    string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+    string = re.sub(r"\s{2,}", " ", string)
     return string.strip().lower()
 
-if __name__=="__main__":    
-    w2v_file = sys.argv[1]     
-    data_folder = ["rt-polarity.pos","rt-polarity.neg"]    
-    print "loading data...",        
-    revs, vocab = build_data_cv(data_folder, cv=10, clean_string=True)
-    max_l = np.max(pd.DataFrame(revs)["num_words"])
+def main():
+    if len(sys.argv) < 3:
+        print "Usage: python process_data.py [data file] [w2v file]"
+        sys.exit("Not enough arguments")
+    data_filename = sys.argv[1]
+    w2v_filename = sys.argv[2]
+    print "loading data...",
+    quos, vocab = build_data_cv(data_filename) #all the quotes and all the words as the dictionary vocab
+    max_l = np.max(pd.DataFrame(quos)["num_words"]) #max length of all the examples
     print "data loaded!"
-    print "number of sentences: " + str(len(revs))
+    print "number of quotes: " + str(len(quos))
     print "vocab size: " + str(len(vocab))
     print "max sentence length: " + str(max_l)
     print "loading word2vec vectors...",
-    w2v = load_bin_vec(w2v_file, vocab)
+    w2v = load_bin_vec(w2v_filename, vocab) #dictionary contains the
     print "word2vec loaded!"
-    print "num words already in word2vec: " + str(len(w2v))
+    print "num words already in word2vec: " + str(len(w2v)) #the original length
     add_unknown_words(w2v, vocab)
-    W, word_idx_map = get_W(w2v)
+    W, word_idx_map = get_W(w2v) # using word vectors from mikolov, while word not in the bin are randomly initialized
     rand_vecs = {}
     add_unknown_words(rand_vecs, vocab)
-    W2, _ = get_W(rand_vecs)
-    cPickle.dump([revs, W, W2, word_idx_map, vocab], open("mr.p", "wb"))
+    W2, _ = get_W(rand_vecs) # all words are of randomized vectors
+    cpcl.dump([quos, W, W2, word_idx_map, vocab], open("data4use.dat", "wb"))
     print "dataset created!"
-    
+
+
+if __name__ == "__main__":
+    main()
